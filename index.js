80585f823f7598bd11733558150d67dceb57b475
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2023-01-29 18:56:10 +0600

    delete done

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
 
         })
 
+        app.delete('/delete-billing/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await billingCollection.deleteOne(query);
+            res.send(result);
+
+        })
+
         // app.get('/products/:categoryID', async (req, res) => {
         //     const id = req.params.categoryID;
         //     const query = { category_id: parseInt(id) };
